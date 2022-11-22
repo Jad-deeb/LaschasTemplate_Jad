ee96ed7ea89b7276598736dfb7b0b8ffa396ac9d
Jad-deeb <jad.deeb@outlook.com>
2022-11-22 00:51:12 +0000

    second commit

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 // draw a filled in rect
 void drawRect(ScreenPixelData* pixelData, int xOffset, int yOffset, int width, int height, unsigned int color);
 void drawCircleSimple(ScreenPixelData* pixelData, int xOffset, int yOffset, int radius, unsigned int color);
-void drawCircleBresenham(ScreenPixelData* pixelData, int xOffset, int yOffset, int radius, unsigned int color);
+void drawCricleMidPoint(ScreenPixelData* pixelData, int xOffset, int yOffset, int radius, unsigned int color = -1);
 
 // init window width and height, keeping it outside to be accessible in functions if needed
 unsigned int windowWidth = 400;
@@ -33,7 +33,8 @@ int main(int argc, char* argv[])
 
     ScreenPixelData* pixelData = wnd1.getPixelData();
     //drawRect(pixelData, 10, 10, 200, 200, );
-    drawCircleSimple(pixelData, 200, 200, 100, 0xFF5733);
+    //drawCircleSimple(pixelData, 200, 200, 100, 0xFF5733);
+    drawCricleMidPoint(pixelData, 175, 175, 150);
     
     Sleep(10);
 
@@ -77,10 +78,49 @@ void drawCircleSimple(ScreenPixelData* pixelData, int xOffset, int yOffset, int 
     }
 }
 
+
 /*
- * bitshifting a binary to the left is the same as mult2 wtf ? left bitshifting rad makes diameter..
+ * sauce : https://www.geeksforgeeks.org/mid-point-circle-drawing-algorithm/
+ * I was today years old when I learned what an octant is, 1/8th of a circumference. -- I'd say it's average kek.
+ * So we draw a single octant, and then copy pasta it x amount of times.
+ * We draw a point then compare to see if that points is within the perimeter or not, if not, we skip it, if it is, we draw it.
+ * Can't tell you I understand the formula that well, but I understand the general idea behind it :)
  */
-void drawCircleBresenham(ScreenPixelData* pixelData, int xOffset, int yOffset, int radius, unsigned int color)
+void drawCricleMidPoint(ScreenPixelData* pixelData, int xOffset, int yOffset, int radius, unsigned int color)
 {
-	
+    int x = radius, y = 0;
+    int P = 1 - radius; // first point (radius,0)
+
+    while (x>y)
+    {
+        y++;
+
+        if (P <= 0) // point inside or on perim
+        {
+            P = P + 2 * y + 1;
+        }
+        else
+        {
+            x--;
+            P = P + 2 * y - 2 * x + 1;
+        }
+
+        // stop when all is printed
+        if (x < y)
+            break;
+
+        pixelData->data[xOffset + x + (yOffset + y) * pixelData->pitch / 4] = 0xFF5733;
+        pixelData->data[xOffset + -x + (yOffset + y) * pixelData->pitch / 4] = 0x00FF33;
+        pixelData->data[xOffset + x + (yOffset + -y) * pixelData->pitch / 4] = 0xF057FF;
+        pixelData->data[xOffset + -x + (yOffset + -y) * pixelData->pitch / 4] = 0x0000FF;
+
+        if (x != y) // don't redraw points
+        {
+            pixelData->data[xOffset + y + (yOffset + x) * pixelData->pitch / 4] = 0x00FF33;
+            pixelData->data[xOffset + -y + (yOffset + x) * pixelData->pitch / 4] = 0xFF5733;
+            pixelData->data[xOffset + y + (yOffset + -x) * pixelData->pitch / 4] = 0x0000FF;
+            pixelData->data[xOffset + -y + (yOffset + -x) * pixelData->pitch / 4] = 0xF057FF;
+        }
+
+    }
 }
